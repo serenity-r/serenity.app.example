c5a7535e14068b3a060f8d710949361b2ff0a50c
M. Drew LaMar <mdlama@wm.edu>
2019-02-12 14:16:08 -0500

    [feat] Added statistical tests

--- inst/tusklessness/tusklessness.R
+++ inst/tusklessness/tusklessness.R
@@ -20,7 +20,7 @@ library(tusklessness)
 library(readr)
 
 # HTML(markdown::markdownToHTML(knitr::knit(system.file("vignettes/teaching_module.Rmd", package = "tusklessness"), quiet = TRUE)))
-teaching_mods <- c("teaching_module.md", "q1a.md", "loremipsum.md")
+teaching_mods <- c("teaching_module.md", "q1a.md")
 
 dataset <- elephantMorphometricsAndTuskSize %>%
   mutate(Sex = as.factor(Sex),
@@ -38,12 +38,103 @@ ui <- dashboardPage(
   dashboardHeader(title = "Tusklessness"),
   dashboardSidebar(
     sidebarMenu(
+      id = "sidebar",
       menuItem("Module", tabName = "module", icon = icon("chalkboard-teacher")),
       menuItem("Data", tabName = "data", icon = icon("table")),
       menuItem("Explore", tabName = "explore", icon = icon("chart-area")),
-      menuItem("Analyze", tabName = "analyze", icon = icon("calculator"))
+      menuItem("Analyze",
+               tabName = "analyze",
+               icon = icon("calculator"),
+               menuSubItem("Statistical Summaries",
+                           tabName = "summaries"),
+               menuSubItem("Hypothesis Tests",
+                           tabName = "tests")
+      )
     ),
-    downloadButton('download', 'Download')
+    hr(),
+    inputPanel(
+    conditionalPanel(condition = "input.sidebar == 'module'",
+                     downloadButton('download', 'Download')
+    ),
+    conditionalPanel(condition = "input.sidebar == 'summaries'",
+                     selectizeInput(
+                       "variable",
+                       "Compute statistics for:",
+                       choices = dataset_num,
+                       multiple = TRUE,
+                       options = list(
+                         'plugins' = list('remove_button'),
+                         'create' = TRUE,
+                         'persist' = FALSE
+                       )
+                     ),
+                     selectizeInput(
+                       "groupby",
+                       "Group by:",
+                       choices = dataset_cat,
+                       multiple = TRUE,
+                       options = list(
+                         'plugins' = list('remove_button',
+                                          'drag_drop'),
+                         'create' = TRUE,
+                         'persist' = FALSE
+                       )
+                     ),
+                     selectizeInput(
+                       "statistics",
+                       "Statistics:",
+                       choices = c("Min" = "min",
+                                   "1st Qu." = "firstquartile",
+                                   "Median" = "median",
+                                   "Mean" = "mean",
+                                   "3rd Qu." = "thirdquartile",
+                                   "Max" = "max"),
+                       multiple = TRUE,
+                       selected = c("min", "firstquartile", "median", "mean", "thirdquartile", "max"),
+                       options = list(
+                         'plugins' = list('remove_button',
+                                          'drag_drop'),
+                         'create' = TRUE,
+                         'persist' = FALSE
+                       )
+                     )
+    ),
+    conditionalPanel(condition = "input.sidebar == 'tests'",
+                     selectInput(
+                       "tests",
+                       "Please select a test:",
+                       choices = c("One-Sample T-Test" = "onesample",
+                                   "Two-Sample T-Test" = "twosample",
+                                   "ANOVA" = "anova",
+                                   "Linear Regression" = "regression")
+                     ),
+                     conditionalPanel(condition = "input.tests == 'onesample'",
+                                      selectInput(
+                                        "onesample_var",
+                                        "Variable:",
+                                        choices = dataset_num
+                                        ),
+                                      numericInput(
+                                        "onesample_null",
+                                        "Null Value:",
+                                        value = 0
+                                        ),
+                                      selectInput(
+                                        "onesample_side",
+                                        "One or Two Sided:",
+                                        choices = c("Two sided" = "two.sided",
+                                                    "One sided: Less" = "less",
+                                                    "One sided: Greater" = "greater")
+                                      ),
+                                      sliderInput(
+                                        "onesample_alpha",
+                                        "Significance Level:",
+                                        value = 0.05,
+                                        min = 0, max = 1, step = 0.01
+                                      )
+                     )
+    )
+    )
   ),
   dashboardBody(
     tabItems(
@@ -54,76 +145,23 @@ ui <- dashboardPage(
               includeMarkdown(system.file("www/loremipsum.md", package = "tusklessness")),
               textAreaInput("q1a", NULL, width="600px", rows = 6, placeholder = "Enter answer here..."),
               actionButton("getPlot", "Grab Plot"),
-              plotOutput("myplot", width = "50%")
+              plotOutput("myplot", width = "50%"),
+              actionButton("getTable", "Grab Table"),
+              DTOutput('mytable')
       ),
-
-      # Second tab content
       tabItem(tabName = "data",
               DTOutput('table')
       ),
-
-      # Third tab content
       tabItem(tabName = "explore",
               serenityVizUI(id = "explore", dataset = dataset, showcode = FALSE, height="700px")
       ),
-
-      # Third tab content
-      tabItem(tabName = "analyze",
-              tabBox(
-                id = "analysis",
-                width = 12,
-                tabPanel("Summary Statistics",
-                         fillPage(
-                           fillRow(
-                             flex = c(2, 6),
-                             wellPanel(
-                               height = "100%",
-                               selectizeInput(
-                                 "variable",
-                                 "Compute statistics for:",
-                                 choices = dataset_num,
-                                 multiple = TRUE,
-                                 options = list(
-                                   'plugins' = list('remove_button'),
-                                   'create' = TRUE,
-                                   'persist' = FALSE
-                                 )
-                               ),
-                               selectizeInput(
-                                 "groupby",
-                                 "Group by:",
-                                 choices = dataset_cat,
-                                 multiple = TRUE,
-                                 options = list(
-                                   'plugins' = list('remove_button',
-                                                    'drag_drop'),
-                                   'create' = TRUE,
-                                   'persist' = FALSE
-                                 )
-                               ),
-                               selectizeInput(
-                                 "statistics",
-                                 "Statistics:",
-                                 choices = c("Min" = "min",
-                                             "1st Qu." = "firstquartile",
-                                             "Median" = "median",
-                                             "Mean" = "mean",
-                                             "3rd Qu." = "thirdquartile",
-                                             "Max" = "max"),
-                                 multiple = TRUE,
-                                 selected = c("min", "firstquartile", "median", "mean", "thirdquartile", "max"),
-                                 options = list(
-                                   'plugins' = list('remove_button',
-                                                    'drag_drop'),
-                                   'create' = TRUE,
-                                   'persist' = FALSE
-                                 )
-                               )
-                             ),
-                             DTOutput('statsummary')
-                           ))
-                         ),
-                tabPanel("Statistical Tests")
+      tabItem(tabName = "summaries",
+              DTOutput('statsummary')
+      ),
+      tabItem(tabName = "tests",
+              fillRow(
+                plotOutput("tests_plot"),
+                verbatimTextOutput("tests_results")
               )
       )
     )
@@ -137,6 +175,7 @@ server <- function(input, output, session) {
                              id = "explore",
                              dataset = dataset)
 
+  # User plots ----
   plots <- reactiveValues(myplot_text = NULL)
 
   observeEvent(input$getPlot, {
@@ -148,6 +187,19 @@ server <- function(input, output, session) {
     eval(parse(text=plots$myplot_text))
   })
 
+  # User tables ----
+  tables <- reactiveValues(mytab = NULL)
+
+  observeEvent(input$getTable, {
+    tables$mytab <- DTstatsummary()
+  }, ignoreInit = TRUE)
+
+  output$mytable <- DT::renderDataTable({
+    req(tables$mytab)
+    tables$mytab
+  })
+
+  # Main Data Table ----
   output$table <- renderDT(
     dataset,
     filter = "top",
@@ -157,6 +209,36 @@ server <- function(input, output, session) {
     options = list(server = FALSE)
     )
 
+  # Statistical Tests ----
+  onesample <- reactive({
+    t.test(dataset[[input$onesample_var]],
+           alternative = input$onesample_side,
+           mu = input$onesample_null,
+           conf.level = 1-input$onesample_alpha)
+  })
+
+  output$tests_plot <- renderPlot({
+    if (input$tests == "onesample") {
+      dataset %>%
+        ggplot(aes(x = 1, y = !!sym(input$onesample_var))) +
+        geom_point(position = position_jitter(width = 0.1, seed = 42)) +
+        scale_x_continuous(limits = c(0, 2)) +
+        geom_hline(yintercept = input$onesample_null,
+                   linetype = 2,
+                   color = "red") +
+        geom_point(aes(x = 1.25, y = onesample()$estimate)) +
+        geom_errorbar(aes(x = 1.25,
+                          ymin = onesample()$conf.int[1],
+                          ymax = onesample()$conf.int[2]),
+                      width = 0.05)
+    }
+  })
+
+  output$tests_results <- renderPrint({
+    onesample()
+  })
+
+  # Summary statistics ----
   statsummary <- reactive({
     req(input$variable, input$groupby)
 
@@ -185,7 +267,7 @@ server <- function(input, output, session) {
     statsum
   })
 
-  output$statsummary <- DT::renderDataTable({
+  DTstatsummary <- reactive({
     # In case you want to control for # decimal digits in future:
     # https://groups.google.com/forum/#!topic/shiny-discuss/2jlYOYFp2-A
 
@@ -207,6 +289,10 @@ server <- function(input, output, session) {
     ) %>% DT::formatStyle(cn_cat,
                           color = "white",
                           backgroundColor = "grey")
+  })
+
+  output$statsummary <- DT::renderDataTable({
+    DTstatsummary()
   })
 
   # downloadHandler() takes two arguments, both functions.
@@ -232,6 +318,7 @@ server <- function(input, output, session) {
         plotrmd <- paste(c("\n```{r, echo=FALSE, warning=FALSE}", paste(plots$myplot_text, collapse = "\n"), "```\n"), collapse = "\n")
         content <- paste(c(content, plotrmd), collapse = "\n")
       }
+
       cat(content, file = tmp)
       rmarkdown::render(tmp, output_file = file)
     }
