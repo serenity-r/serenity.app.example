d51b21b357a673f29ee23d9211721abd33428525
M. Drew LaMar <mdlama@wm.edu>
2019-02-15 12:00:05 -0500

    [feat] Beginnings of two-sample t-test

--- app.R
+++ app.R
@@ -35,6 +35,7 @@ dataset_cat <- dataset_all[-which(dataset_all %in% dataset_num)] # setdiff?
 
 ui <- dashboardPage(
   dashboardHeader(title = "Tusklessness"),
+  # Dashboard Sidebar ----
   dashboardSidebar(
     sidebarMenu(
       id = "sidebar",
@@ -128,11 +129,42 @@ ui <- dashboardPage(
                                         "onesample_alpha",
                                         "Significance Level:",
                                         value = 0.05,
-                                        min = 0, max = 1, step = 0.01
+                                        min = 0.01, max = 1, step = 0.01
+                                      )
+                     ),
+                     conditionalPanel(condition = "input.tests == 'twosample'",
+                                      selectInput(
+                                        "twosample_exp",
+                                        "Explanatory Variable:",
+                                        choices = dataset_cat
+                                      ),
+                                      selectInput(
+                                        "twosample_res",
+                                        "Response Variable:",
+                                        choices = dataset_num
+                                      ),
+                                      numericInput(
+                                        "twosample_null",
+                                        "Null Value:",
+                                        value = 0
+                                      ),
+                                      selectInput(
+                                        "twosample_side",
+                                        "One or Two Sided:",
+                                        choices = c("Two sided" = "two.sided",
+                                                    "One sided: Less" = "less",
+                                                    "One sided: Greater" = "greater")
+                                      ),
+                                      sliderInput(
+                                        "twosample_alpha",
+                                        "Significance Level:",
+                                        value = 0.05,
+                                        min = 0.01, max = 1, step = 0.01
                                       )
                      )
     )
   ),
+  # Dashboard Body ----
   dashboardBody(
     tabItems(
       # First tab content
@@ -149,10 +181,14 @@ ui <- dashboardPage(
               DTOutput('statsummary')
       ),
       tabItem(tabName = "tests",
-              fillRow(
-                plotOutput("tests_plot"),
-                verbatimTextOutput("tests_results")
-              )
+              box(title = "Visualization",
+                  status = "primary",
+                  solidHeader = TRUE,
+                  plotOutput("tests_plot")),
+              box(title = "Results",
+                  status = "primary",
+                  solidHeader = TRUE,
+                  verbatimTextOutput("tests_results"))
       )
     )
   ),
@@ -207,25 +243,73 @@ server <- function(input, output, session) {
            conf.level = 1-input$onesample_alpha)
   })
 
+  twosample <- reactive({
+    t.test(dataset[[input$twosample_res]] ~ dataset[[input$twosample_exp]],
+           alternative = input$twosample_side,
+           mu = input$twosample_null,
+           conf.level = 1-input$twosample_alpha)
+  })
+
+  onesample_plot <- reactive({
+    dataset %>%
+      ggplot(aes(x = !!sym(input$onesample_var))) +
+      geom_histogram(colour = "black", fill = "gray72") +
+      geom_errorbarh(aes(xmin = onesample()$conf.int[1],
+                         xmax = onesample()$conf.int[2],
+                         y = 0),
+                     colour = "blue",
+                     size = 1.2,
+                     height=2) +
+      geom_point(x = onesample()$estimate,
+                 y = 0,
+                 size = 2,
+                 colour = "green") +
+      geom_point(x = input$onesample_null,
+                 y = 0,
+                 size = 2,
+                 colour = "red") +
+      expand_limits(x = input$onesample_null)
+  })
+
+  twosample_plot <- reactive({
+    conf_data <- dataset %>%
+      group_by(!!sym(input$twosample_exp)) %>%
+      summarize(n = n(),
+                x = mean(!!sym(input$twosample_res), na.rm=T),
+                s = sd(!!sym(input$twosample_res), na.rm=T),
+                se = s/sqrt(n),
+                tcrit = qt(p = 1-(input$twosample_alpha/2), df = n-1),
+                low = x - tcrit*se,
+                high = x + tcrit*se)
+    dataset %>%
+      ggplot(aes(x = !!sym(input$twosample_exp),
+                 y = !!sym(input$twosample_res))) +
+      geom_point(position = position_jitter(0.1),
+                 alpha = 0.5) +
+      geom_errorbar(aes(y = NULL, ymin = low, ymax = high),
+                    position = position_nudge(0.2),
+                    width = 0.05,
+                    size = 1.2,
+                    colour = "blue",
+                    data = conf_data)
+  })
+
   output$tests_plot <- renderPlot({
     if (input$tests == "onesample") {
-      dataset %>%
-        ggplot(aes(x = 1, y = !!sym(input$onesample_var))) +
-        geom_point(position = position_jitter(width = 0.1, seed = 42)) +
-        scale_x_continuous(limits = c(0, 2)) +
-        geom_hline(yintercept = input$onesample_null,
-                   linetype = 2,
-                   color = "red") +
-        geom_point(aes(x = 1.25, y = onesample()$estimate)) +
-        geom_errorbar(aes(x = 1.25,
-                          ymin = onesample()$conf.int[1],
-                          ymax = onesample()$conf.int[2]),
-                      width = 0.05)
-    }
+      onesample_plot()
+    } else
+      if (input$tests == "twosample") {
+        twosample_plot()
+      }
   })
 
   output$tests_results <- renderPrint({
-    onesample()
+    if (input$tests == "onesample") {
+      onesample()
+    } else
+      if (input$tests == "twosample") {
+        twosample()
+      }
   })
 
   # Summary statistics ----
